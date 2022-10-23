a9fadbc4dfed47aede46a80754de0fac4145a4ad
Leibruk <mateusz.kurbiel@gmail.com>
2022-10-23 13:37:37 +0200

    add-authentication-for-login

--- registration_login/views.py
+++ registration_login/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import CreateUserForm
 
 from django.contrib import messages
-
+from django.contrib.auth import authenticate, login, logout
 
 
 def registerPage(request):
@@ -20,5 +20,17 @@ def registerPage(request):
     return render(request, 'registration_login/register.html', context)
 
 def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('')
+        else:
+            messages.info(request, 'Nazwa użytkownika lub hasło jest niepoprawne')
+
     context = {}
     return render(request, 'registration_login/login.html', context)
