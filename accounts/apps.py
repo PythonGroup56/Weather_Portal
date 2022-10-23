09dac8dcd3f6850a0d09ee4dcd6393c6080802a2
Leibruk <mateusz.kurbiel@gmail.com>
2022-10-23 15:26:15 +0200

    After codereview, change appname

--- accounts/apps.py
+++ accounts/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class RegistrationLoginConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "registration_login"
+    name = "accounts"
